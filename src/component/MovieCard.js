5e99712c2f620a81c702452a7ad12a615f9229cf
Brijesh Singh <bsingh6636@gmail.com>
2024-04-25 13:15:34 +0530

    env

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -2,9 +2,8 @@ import React from "react";
 import { tmdbimage_url } from "../utils/Const";
 
 const MovieCard = ({ poster_path }) => {
-   console.log(poster_path)
-  if(poster_path.poster_path === null || undefined) return;
-   console.log(poster_path)
+  if (poster_path.poster_path === null || undefined) return;
+
   return (
     <div className="w-44 px-3 cursor-pointer hover:w-[195px] transition-all duration-500 ease-in-out transform hover:scale-105">
       <img
